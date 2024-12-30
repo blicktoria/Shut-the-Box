086f320672e3421c956fcc07a69dd99f88bd1c7b
victoria <piggivicki@gmail.com>
2024-12-29 20:49:37 -0500

    Got rid of roll2 method, just rolling 2 Die objects instead. Added Javadoc and comments

--- LeeVicShutBoxReal/src/lee/victoria/Die.java
+++ LeeVicShutBoxReal/src/lee/victoria/Die.java
@@ -1,38 +1,39 @@
 package lee.victoria;
 import java.util.Random;
-
+/**
+ * Purpose: Create Die objects to be rolled in the Shut the Box game
+ * @author V. Lee
+ * @date December 29 2024
+ */
 public class Die {
 	
-	private int value;
+	//private int value;
 	private int numSides;
 	Random random = new Random();
 	
 	/**
-	 * Purpose: Cdosjdsf
-	 * @param n
+	 * Purpose: When an integer value of sides is provided, initialize a Die object with n sides
+	 * @param n int number of Die sides
 	 */
 	public Die(int n) {
 		this.numSides = n;
-		this.value = roll();
+		//this.value = roll();
 	}
 	/**
-	 * 
+	 * Purpose: When no other parameters are provided, initialize a Die object with 6 sides 
 	 */
 	public Die() {
 		this.numSides = 6;
-		this.value = roll();
+		//this.value = roll();
 	}
-	//test test test
-	//test test II
+	
+	
 	/**
-	 * 
-	 * @return
+	 * Purpose: to roll a Die object
+	 * @return Random integer of a certain range, depending on the number of sides
 	 */
 	public int roll() {
 		return random.nextInt(numSides)+1;
 	}
-	public int roll2() {
-		//rolling a minimum of 2, maximum of 12
-		return random.nextInt(11)+2;
-	}
+	
 }
